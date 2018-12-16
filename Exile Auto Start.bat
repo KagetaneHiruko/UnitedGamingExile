2dd0bf815fecb88a7108fac73c734c964b4ba758
Tim Wieckhorst <Tim.Wieckhorst@t-online.de>
2018-12-16 13:12:19 +0100

    Initial Release (CONFIGS MUST BE OVERLOOKED)

--- Exile Auto Start.bat
+++ Exile Auto Start.bat
@@ -3,10 +3,10 @@ color 0a
 title Exile Monitor
 :Serverstart
 echo Launching Server
-C:
-cd "C:\Users\Tim\Desktop\Arma_3_Server"
+S:
+cd "S:\a3exile"
 echo Exile Server Monitor... Active !
-start "Arma3" /min /wait arma3server.exe -mod=@exile;Kart;Mark;Heli; -servermod=@exileserver; -config=C:\Users\Tim\Desktop\Arma_3_Server\@ExileServer\config.cfg -port=2302 -profiles=SC -cfg=C:\Users\Tim\Desktop\Arma_3_Server\@ExileServer\basic.cfg -name=SC -autoinit
+start "Arma3" /min /wait arma3server.exe -mod=@exile;Kart;Mark;Heli;@CBA_A3;@Enhanced_Movement;@Extended_Base_Mod;@Fox_SurvivalCars;@NIArms_All_in_One;@Zombies_and_Demons;  -servermod=@exileserver; -config=S:\a3exile\@ExileServer\config.cfg -port=2302 -profiles=SC -cfg=S:\a3exile\@ExileServer\basic.cfg -name=SC -autoinit
 ping 127.0.0.1 -n 15 >NUL
 echo Exile Server Shutdown ... Restarting!
 ping 127.0.0.1 -n 5 >NUL
--- mpmissions/Exile.Tanoa/config.cpp
+++ mpmissions/Exile.Tanoa/config.cpp
@@ -3358,7 +3358,20 @@ class CfgExileArsenal
 
 class CfgExileCustomCode 
 {
-	#include "CfgExileCustomCode.cpp"
+	/*
+		You can overwrite every single file of our code without touching it.
+		To do that, add the function name you want to overwrite plus the 
+		path to your custom file here. If you wonder how this works, have a
+		look at our bootstrap/fn_preInit.sqf function.
+
+		Simply add the following scheme here:
+
+		<Function Name of Exile> = "<New File Name>";
+
+		Example:
+
+		ExileClient_util_fusRoDah = "myaddon\myfunction.sqf";
+	*/
 };
 class CfgExileEnvironment
 {
